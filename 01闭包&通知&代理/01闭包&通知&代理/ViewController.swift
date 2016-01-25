de0d468b42daa23f6d52c06d27284ae86e06b199
4jchc <jiangjin5y24@163.com>
2016-01-25 14:16:26 +0800

    【01闭包&通知&代理】01初始化

---
+++ 01闭包&通知&代理/01闭包&通知&代理/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  01闭包&通知&代理
+//
+//  Created by 蒋进 on 16/1/25.
+//  Copyright © 2016年 蒋进. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
